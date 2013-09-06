4a545a3b621a101c2ac628d81698edfe3012a57f
Bryce Mehring <bm4g5@mst.edu>
2013-09-06 00:32:07 -0500

    Added spaceship flying around with 3D space effect

--- source/adventure.cpp
+++ source/adventure.cpp
@@ -1,10 +1,12 @@
-#define PLUGIN_EXPORTS
-
 #include "adventure.h"
 #include "IRenderer.h"
 #include "IKMInput.h"
+#include "Game.h"
+#include "Camera.h"
 
-RTTI_IMPL(adventure)
+#include <sstream>
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
 
 extern "C" IPlugin* CreatePlugin()
 {
@@ -12,25 +14,48 @@ extern "C" IPlugin* CreatePlugin()
 }
 
 // Called only once when the plugin is created
-void adventure::Init(IGame& game)
+void adventure::Init(Game& game)
 {
+	m_pCamera = CreateCamera();
+
+	int width, height;
+	game.GetRenderer().GetDisplayMode(game.GetRenderer().GetCurrentDisplayMode(),width,height);
+	m_pCamera->setLens(90.0f,width,height,1.0f,5000.0f);
+	m_pCamera->lookAt(glm::vec3(0.0f,0.0f,100.0f),glm::vec3(0.0f,0.0f,0.0f),glm::vec3(0.0f,1.0f,0.0f));
+	m_pCamera->update();
+
+	m_pCamera->AddRef();
+	game.GetRenderer().SetCamera(m_pCamera);
 }
 
 // Called only once when the plugin is destroyed
-void adventure::Destroy(IGame& game)
+void adventure::Destroy(Game& game)
 {
-
+	ReleaseCamera(m_pCamera);
 }
 
 // Called every frame to update the date of the game
-void adventure::Update(IGame& game)
+void adventure::Update(Game& game)
 {
-
+	m_spaceShip.Update(game.GetDt(),m_pCamera,game.GetInput());
 }
 
 // Called every frame to render the game
-void adventure::Draw(IGame& game)
+void adventure::Draw(Game& game)
 {
-	game.GetRenderer().DrawString("Adventure",glm::vec3(0.0f,0.0f,-20.0f),glm::vec2(10.0f),glm::vec3(0.0f,0.0f,0.0f),0,FontAlignment::Center);
+	IRenderer& renderer = game.GetRenderer();
+
+	int zPos = -800;
+	for(int i = 0; i < 10; ++i)
+	{
+		glm::mat4 T = glm::translate(0.0f,0.0f,(float)zPos);
+		T = glm::scale(T,8000.0f,8000.0f,1.0f);
+		renderer.DrawSprite("stars",T,glm::vec2(30.0f /(i + 1),30.0f / (i + 1))); // 15
+
+		zPos += 50;
+	}
+
+
+	m_spaceShip.Render(renderer);
 }
 
--- source/adventure.h
+++ source/adventure.h
@@ -2,13 +2,12 @@
 #define _ADVENTURE_
 
 #include "IGameState.h"
-#include "RTTI.h"
+#include "space_ship.h"
+#include "Camera.h"
 
 class adventure : public IGameState
 {
 public:
-
-	RTTI_DECL
 
 	// returns the type of the plug-in
 	virtual DLLType GetPluginType() const { return DLLType::Game; }
@@ -22,18 +21,22 @@ public:
 	virtual void Init(class asIScriptEngine*) {}
 	virtual void Destroy(class asIScriptEngine*) {}
 
-
 	// Called only once when the plugin is created
-	virtual void Init(IGame& game);
+	virtual void Init(Game& game);
 
 	// Called only once when the plugin is destroyed
-	virtual void Destroy(IGame& game);
+	virtual void Destroy(Game& game);
 
 	// Called every frame to update the date of the game
-	virtual void Update(IGame& game);
+	virtual void Update(Game& game);
 
 	// Called every frame to render the game
-	virtual void Draw(IGame& game);
+	virtual void Draw(Game& game);
+
+private:
+
+	UserControlledSpaceShip m_spaceShip;
+	Camera* m_pCamera;
 };
 
 #endif // _ADVENTURE_
