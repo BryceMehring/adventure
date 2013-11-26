a497efe767f87923885e34d00d66a329cb67246d
Bryce Mehring <bm4g5@mst.edu>
2013-11-25 22:42:05 -0600

    Added missing texture

--- plugin/adventure/adventure.r
+++ plugin/adventure/adventure.r
@@ -3,5 +3,4 @@ animation squid textures/squid.png
 animation weapon textures/weapon.png
 animation explosion textures/explosion.png
 texture stars textures/stars.png
-texture slider textures/slider.png
 
